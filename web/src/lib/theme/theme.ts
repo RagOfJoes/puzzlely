151b3ad0a9e0e0a88c0cc407f031753d6a0f5bc5
RagOfJoes <apps@ragofjoes.dev>
2023-04-05 10:32:33 -0700

    chore(web): cleanup files

--- web/src/lib/theme/theme.ts
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from '@chakra-ui/react';
-
-import components from './components';
-import foundations from './foundations';
-import global from './global';
-
-const theme = extendTheme(foundations, global, components);
-
-export default theme;
