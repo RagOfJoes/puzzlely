e2d0266fa534382697b0586c1f8b41c2d34dad0c
RagOfJoes <apps@ragofjoes.dev>
2023-04-14 10:48:02 -0700

    revert(web): commented out CSP

--- web/next.config.js
+++ web/next.config.js
@@ -1,36 +1,36 @@
 // Before defining your Security Headers
 // add Content Security Policy directives using a template string.
-// const ContentSecurityPolicy = `
-//   default-src 'self' www.puzzlely.io api.puzzlely.io;
-//   child-src puzzlely.io;
-//   connect-src api.puzzlely.io www.puzzlely.io api.panelbear.com;
-//   script-src 'self';
-//   style-src 'self' 'unsafe-inline';
-//   font-src 'self';
-// `;
+const ContentSecurityPolicy = `
+  default-src 'self' www.puzzlely.io api.puzzlely.io;
+  child-src puzzlely.io;
+  connect-src api.puzzlely.io www.puzzlely.io api.panelbear.com;
+  script-src 'self';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+`;
 
-// const securityHeaders = [
-//   {
-//     key: "Content-Security-Policy",
-//     value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-//   },
-//   {
-//     key: "X-Content-Type-Options",
-//     value: "nosniff",
-//   },
-//   {
-//     key: "X-DNS-Prefetch-Control",
-//     value: "on",
-//   },
-//   {
-//     key: "X-Frame-Options",
-//     value: "SAMEORIGIN",
-//   },
-//   {
-//     key: "X-XSS-PROTECTION",
-//     value: "1; mode=block",
-//   },
-// ];
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-XSS-PROTECTION",
+    value: "1; mode=block",
+  },
+];
 
 module.exports = {
   // The starter code load resources from `public` folder with `router.basePath`
@@ -49,11 +49,11 @@ module.exports = {
     }
 
     return [
-      // {
-      //   // Apply these headers to all routes
-      //   source: "/:path*",
-      //   headers: securityHeaders,
-      // },
+      {
+        // Apply these headers to all routes
+        source: "/:path*",
+        headers: securityHeaders,
+      },
     ];
   },
   rewrites: async () => {
