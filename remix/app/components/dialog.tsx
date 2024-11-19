eca313993bb1f8235a9139f3d48078a455b3b2de
RagOfJoes <dev@ragofjoes.dev>
2024-11-18 16:45:39 -0800

    feat(remx): add Dialog and Drawer component

---
+++ remix/app/components/dialog.tsx
@@ -0,0 +1,130 @@
+import type { ComponentPropsWithoutRef, ElementRef, HTMLAttributes } from "react";
+import { forwardRef } from "react";
+
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+
+import { cn } from "@/lib/cn";
+
+export const Dialog = DialogPrimitive.Root;
+
+export const DialogTrigger = DialogPrimitive.Trigger;
+
+export const DialogPortal = DialogPrimitive.Portal;
+
+export const DialogClose = DialogPrimitive.Close;
+
+export const DialogOverlay = forwardRef<
+	ElementRef<typeof DialogPrimitive.Overlay>,
+	ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+	<DialogPrimitive.Overlay
+		ref={ref}
+		className={cn(
+			"fixed inset-0 z-50 bg-background/80",
+
+			"data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
+			"data-[state=open]:animate-in data-[state=open]:fade-in-0",
+
+			className,
+		)}
+		{...props}
+	/>
+));
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
+export const DialogContent = forwardRef<
+	ElementRef<typeof DialogPrimitive.Content>,
+	ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+	<DialogPortal>
+		<DialogOverlay />
+		<DialogPrimitive.Content
+			ref={ref}
+			className={cn(
+				"fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-1 border bg-muted p-4 duration-200",
+
+				"data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
+				"data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+
+				className,
+			)}
+			{...props}
+		>
+			{children}
+
+			<DialogPrimitive.Close
+				className={cn(
+					"absolute right-2 top-2 text-foreground/70 ring-offset-background transition-opacity",
+
+					"data-[state=open]:bg-accent data-[state=open]:text-muted-foreground",
+					"disabled:pointer-events-none",
+					"focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+					"hover:text-foreground/100",
+				)}
+			>
+				<X className="h-4 w-4" />
+				<span className="sr-only">Close</span>
+			</DialogPrimitive.Close>
+		</DialogPrimitive.Content>
+	</DialogPortal>
+));
+DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+export const DialogHeader = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+	<div
+		className={cn(
+			"flex flex-col",
+
+			className,
+		)}
+		{...props}
+	/>
+);
+DialogHeader.displayName = "DialogHeader";
+
+export const DialogFooter = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+	<div
+		className={cn(
+			"flex flex-col-reverse",
+
+			"sm:flex-row sm:justify-end sm:space-x-2",
+
+			className,
+		)}
+		{...props}
+	/>
+);
+DialogFooter.displayName = "DialogFooter";
+
+export const DialogTitle = forwardRef<
+	ElementRef<typeof DialogPrimitive.Title>,
+	ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+	<DialogPrimitive.Title
+		ref={ref}
+		className={cn(
+			"font-heading text-xl font-bold leading-none",
+
+			className,
+		)}
+		{...props}
+	/>
+));
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+export const DialogDescription = forwardRef<
+	ElementRef<typeof DialogPrimitive.Description>,
+	ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+	<DialogPrimitive.Description
+		ref={ref}
+		className={cn(
+			"text-sm font-medium tracking-tight text-muted-foreground",
+
+			className,
+		)}
+		{...props}
+	/>
+));
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
---
+++ remix/app/components/drawer.tsx
@@ -0,0 +1,141 @@
+import type {
+	ComponentProps,
+	ComponentPropsWithoutRef,
+	CSSProperties,
+	ElementRef,
+	HTMLAttributes,
+} from "react";
+import { forwardRef } from "react";
+
+import { Drawer as DrawerPrimitive } from "vaul";
+
+import { cn } from "@/lib/cn";
+
+export const Drawer = ({
+	shouldScaleBackground = true,
+	...props
+}: ComponentProps<typeof DrawerPrimitive.Root>) => (
+	<DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
+);
+Drawer.displayName = "Drawer";
+
+export const DrawerTrigger = DrawerPrimitive.Trigger;
+
+export const DrawerPortal = DrawerPrimitive.Portal;
+
+export const DrawerClose = DrawerPrimitive.Close;
+
+export const DrawerOverlay = forwardRef<
+	ElementRef<typeof DrawerPrimitive.Overlay>,
+	ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+	<DrawerPrimitive.Overlay
+		{...props}
+		className={cn(
+			"fixed inset-0 z-50 bg-background/80",
+
+			className,
+		)}
+		ref={ref}
+	/>
+));
+DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
+
+export const DrawerContent = forwardRef<
+	ElementRef<typeof DrawerPrimitive.Content>,
+	ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+	<DrawerPortal>
+		<DrawerOverlay />
+
+		<DrawerPrimitive.Content
+			{...props}
+			className={cn(
+				"group fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col",
+
+				"data-[vaul-drawer-direction=right]:inset-x-auto data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:bottom-2 data-[vaul-drawer-direction=right]:right-2 data-[vaul-drawer-direction=right]:top-2 data-[vaul-drawer-direction=right]:mt-0 data-[vaul-drawer-direction=right]:w-[310px] data-[vaul-drawer-direction=right]:flex-row",
+
+				className,
+			)}
+			ref={ref}
+			style={{ "--initial-transform": "calc(100% + 8px)" } as CSSProperties}
+		>
+			{children}
+		</DrawerPrimitive.Content>
+	</DrawerPortal>
+));
+DrawerContent.displayName = "DrawerContent";
+
+export const DrawerDragHandler = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+	<div
+		aria-hidden
+		className={cn(
+			"mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted-foreground/40",
+
+			"group-data-[vaul-drawer-direction=right]:hidden",
+			// "group-data-[vaul-drawer-direction=right]:my-auto group-data-[vaul-drawer-direction=right]:ml-2 group-data-[vaul-drawer-direction=right]:mr-0 group-data-[vaul-drawer-direction=right]:h-[100px] group-data-[vaul-drawer-direction=right]:w-2",
+
+			className,
+		)}
+		{...props}
+	/>
+);
+DrawerDragHandler.displayName = "DrawerDragHandler";
+
+export const DrawerHeader = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+	<div
+		{...props}
+		className={cn(
+			"flex flex-col gap-1.5 p-4 text-center",
+
+			"sm:text-left",
+
+			className,
+		)}
+	/>
+);
+DrawerHeader.displayName = "DrawerHeader";
+
+export const DrawerFooter = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+	<div
+		{...props}
+		className={cn(
+			"mt-auto flex flex-col gap-2 p-4",
+
+			className,
+		)}
+	/>
+);
+DrawerFooter.displayName = "DrawerFooter";
+
+export const DrawerTitle = forwardRef<
+	ElementRef<typeof DrawerPrimitive.Title>,
+	ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
+>(({ className, ...props }, ref) => (
+	<DrawerPrimitive.Title
+		{...props}
+		className={cn(
+			"text-lg font-semibold leading-none tracking-tight",
+
+			className,
+		)}
+		ref={ref}
+	/>
+));
+DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
+
+export const DrawerDescription = forwardRef<
+	ElementRef<typeof DrawerPrimitive.Description>,
+	ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>
+>(({ className, ...props }, ref) => (
+	<DrawerPrimitive.Description
+		{...props}
+		className={cn(
+			"text-sm text-muted-foreground",
+
+			className,
+		)}
+		ref={ref}
+	/>
+));
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName;
