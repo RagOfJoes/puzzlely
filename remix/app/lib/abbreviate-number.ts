d1d52d9a02732467e6b27f904d672a37959e6ac4
RagOfJoes <dev@ragofjoes.dev>
2024-11-22 15:54:01 -0800

    feat(remix): add utility functions

---
+++ remix/app/lib/abbreviate-number.ts
@@ -0,0 +1,28 @@
+const REGEX = /\.0+$|(\.[0-9]*[1-9])0+$/;
+
+export function abbreviateNumber(n: number): string {
+	if (n === 0) {
+		return "0";
+	}
+	if (n < 1000) {
+		return n.toString();
+	}
+
+	const item = [
+		{ value: 1e3, symbol: "k" },
+		{ value: 1e6, symbol: "M" },
+		{ value: 1e9, symbol: "G" },
+		{ value: 1e12, symbol: "T" },
+		{ value: 1e15, symbol: "P" },
+		{ value: 1e18, symbol: "E" },
+	]
+		.slice()
+		.reverse()
+		.find((i) => n >= i.value);
+
+	if (!item) {
+		return "0";
+	}
+
+	return (n / item.value).toFixed(1).replace(REGEX, "$1") + item.symbol;
+}
