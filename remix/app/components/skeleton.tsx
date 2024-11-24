36483c494e8fd8fdc98335d66fdf3e1e02fcc8c7
RagOfJoes <dev@ragofjoes.dev>
2024-11-23 17:44:39 -0800

    feat(remix): add Skeleton component

---
+++ remix/app/components/skeleton.tsx
@@ -0,0 +1,27 @@
+import type { ComponentPropsWithoutRef, ElementRef } from "react";
+import { forwardRef } from "react";
+
+import type { Primitive } from "@radix-ui/react-primitive";
+import { Slot } from "@radix-ui/react-slot";
+
+import { cn } from "@/lib/cn";
+
+export const Skeleton = forwardRef<
+	ElementRef<typeof Primitive.div>,
+	ComponentPropsWithoutRef<typeof Primitive.div>
+>(({ asChild, className, ...props }, ref) => {
+	const C = asChild ? Slot : "div";
+
+	return (
+		<C
+			{...props}
+			className={cn(
+				"animate-pulse bg-muted",
+
+				className,
+			)}
+			ref={ref}
+		/>
+	);
+});
+Skeleton.displayName = "Skeleton";
