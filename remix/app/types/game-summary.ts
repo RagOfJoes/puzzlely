05df51fb0ff849cd33c8c41d39623c85e0198ca3
RagOfJoes <dev@ragofjoes.dev>
2024-12-07 18:29:06 -0800

    chore(remix): update type

--- remix/app/types/game-summary.ts
+++ remix/app/types/game-summary.ts
@@ -2,5 +2,7 @@ import type { Game } from "./game";
 import type { PuzzleSummary } from "./puzzle-summary";
 
 export type GameSummary = Omit<Game, "attempts" | "correct" | "puzzle"> & {
+	attempts: number;
+
 	puzzle: PuzzleSummary;
 };
