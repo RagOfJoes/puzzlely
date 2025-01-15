e2a1a703bb2234ea71d9d7ff9af847c205dc8420
RagOfJoes <dev@ragofjoes.dev>
2025-01-15 00:53:01 -0800

    chore(remix): cleanup

--- remix/app/hooks/use-mount.ts
+++
@@ -1,7 +0,0 @@
-import type { EffectCallback } from "react";
-import { useEffect } from "react";
-
-export function useMount(effect: EffectCallback) {
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	useEffect(effect, []);
-}
