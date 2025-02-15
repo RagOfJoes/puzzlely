1b40baa499915e8ebc4e4e57334224cc7ad820be
RagOfJoes <dev@ragofjoes.dev>
2025-02-14 16:16:44 -0800

    fix(api): ensure timezone is set to UTC

--- api/cmd/web/main.go
+++ api/cmd/web/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/RagOfJoes/puzzlely/internal/cmd/web"
 )
+
+func init() {
+	// Set timezone to UTC
+	time.Local = time.UTC
+}
 
 func main() {
 	if err := web.Run(); err != nil {
